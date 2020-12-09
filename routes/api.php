8d43de1f3b33663fa1f95d91a73917a425c21a87
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-12-10 06:43:15 +0900

    done

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LineBotController;
 
@@ -16,8 +16,8 @@ use App\Http\Controllers\LineBotController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::post('/parrot', [LineBotController::class, 'parrot']);
