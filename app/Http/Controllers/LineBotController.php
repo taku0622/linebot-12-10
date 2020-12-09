ce951f0df13633638107520caa8253f077ace80e
Takumi Hayatsu <hayatsutkm@gmail.com>
2020-12-10 06:19:12 +0900

    done

--- app/Http/Controllers/LineBotController.php
+++ app/Http/Controllers/LineBotController.php
@@ -3,13 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use LINE\LINEBot;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LineBotController extends Controller
 {
-    // ---ここから追加---
     public function index()
     {
         return view('linebot.index');
     }
-    // ---ここまで追加---
+
+    public function parrot(Request $request)
+    {
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+
+        $signature = $request->header('x-line-signature');
+
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
+
+        $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+
+        foreach ($events as $event) {
+            if (!($event instanceof TextMessage)) {
+                continue;
+            }
+
+            $replyToken = $event->getReplyToken();
+            $replyText = $event->getText();
+            $lineBot->replyText($replyToken, $replyText);
+        }
+    }
 }
